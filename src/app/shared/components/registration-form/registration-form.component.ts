343ec78e4a7906fc20acbaf49ba9f84624d55b13
SzymonRegula <szymon.regula@gmail.com>
2023-12-16 01:50:57 +0100

    3. Create the complete registration page

--- src/app/shared/components/registration-form/registration-form.component.ts
+++ src/app/shared/components/registration-form/registration-form.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { emailValidator } from '@app/shared/utils/email.validator';
 
 @Component({
   selector: 'app-registration-form',
   templateUrl: './registration-form.component.html',
   styleUrls: ['./registration-form.component.scss'],
 })
-export class RegistrationFormComponent {
+export class RegistrationFormComponent implements OnInit {
   registrationForm!: FormGroup;
+
   // Use the names `name`, `email`, `password` for the form controls.
+  ngOnInit(): void {
+    this.registrationForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      email: new FormControl('', [Validators.required, emailValidator]),
+      password: new FormControl('', [Validators.required]),
+    });
+  }
+
+  onSubmit() {
+    if (this.registrationForm.invalid) {
+      this.registrationForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.registrationForm.value);
+  }
+
+  get name() {
+    return this.registrationForm.get('name');
+  }
+  get email() {
+    return this.registrationForm.get('email');
+  }
+  get password() {
+    return this.registrationForm.get('password');
+  }
 }
